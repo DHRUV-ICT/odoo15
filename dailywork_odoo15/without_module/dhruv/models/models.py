5801c3105e12cb5dcdb4fcf98f9c154e35aed6ea
DHRUV-ICT <patel.dhruv@icreativetechnolabs.com>
2022-02-15 16:59:30 +0530

    [ADD] dailywork_odoo15

---
+++ dailywork_odoo15/without_module/dhruv/models/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class dhruv(models.Model):
+    _name = 'dhruv.dhruv'
+    _description = 'dhruv.dhruv'
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    dhruv = fields.Integer()
+    email = fields.Char(string='Email')
+    abool = fields.Boolean()
+    selection = fields.Selection([('a','A'),('b','B'),('c','C')])
+    abin = fields.Binary()
+    date = fields.Datetime()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
